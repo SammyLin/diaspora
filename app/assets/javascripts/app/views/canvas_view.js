775f216bb40ee744133bbc055797b1f85ef5a6bc
danielgrippi <danielgrippi@gmail.com>
2012-04-11 18:51:21 -0700

    wip

---
+++ app/assets/javascripts/app/views/canvas_view.js
@@ -0,0 +1,15 @@
+app.views.Canvas = app.views.Base.extend({
+
+  templateName : 'canvas',
+
+  postRenderTemplate : function() {
+    setTimeout(_.bind(this.mason, this), 0)
+  },
+
+  mason : function() {
+    this.$el.isotope({
+      itemSelector : '.canvas-frame',
+      columnWidth : 292
+    })
+  }
+})
